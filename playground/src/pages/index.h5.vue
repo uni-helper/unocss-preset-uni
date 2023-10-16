e40cb6575d066e2e0cd9443f176864c12ce9bb07
kejun <kejun1997@gmail.com>
2023-10-16 23:37:28 +0800

    feat: add platform variant

--- playground/src/pages/index.h5.vue
+++ playground/src/pages/index.h5.vue
@@ -1,5 +1,7 @@
 <template>
   <div container mx-auto py-10>
-    <view class="bg-blue cursor-pointer border-1 inline-block p-2 rounded">h5 page</view>
+    <view class=" cursor-pointer border-1 inline-block p-2 rounded uni-h5:bg-red">
+      h5 page
+    </view>
   </div>
 </template>
---
+++ src/variants.ts
@@ -0,0 +1,27 @@
+import type { VariantContext, VariantObject } from 'unocss'
+import { h } from '@unocss/preset-mini/utils'
+import { platform } from '@uni-helper/uni-env'
+import { variantGetParameter } from '@unocss/rule-utils'
+
+export function createVariants() {
+  const platformVariants: VariantObject = {
+    name: 'platform',
+    match(matcher: string, ctx: Readonly<VariantContext<object>>) {
+      const variant = variantGetParameter('uni-', matcher, ctx.generator.config.separators)
+      if (variant) {
+        const [match, rest] = variant
+        let matchPlatform = h.bracket(match) ?? ''
+        if (matchPlatform === '')
+          matchPlatform = (ctx.theme as any).platform?.[match] ?? ''
+        if (platform && platform.startsWith(matchPlatform)) {
+          return {
+            matcher: rest,
+            selector: s => s,
+          }
+        }
+      }
+    },
+  }
+
+  return [platformVariants]
+}
