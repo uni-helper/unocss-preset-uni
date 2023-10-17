2cf2bad769fa6c7031c61fe04e82f51c1fd7af6e
KeJunMao <kejun1997@gmail.com>
2023-10-17 13:51:15 +0800

    feat: support autocomplete

--- src/variants.ts
+++ src/variants.ts
@@ -6,22 +6,22 @@ import type { Theme } from '@unocss/preset-mini'
 
 export function createVariants<T extends object = Theme>() {
   const platformVariants: VariantObject<T> = {
-    name: 'platform',
+    name: 'platforms',
     match(matcher: string, ctx: Readonly<VariantContext<T>>) {
       const variant = variantGetParameter('uni-', matcher, ctx.generator.config.separators)
       if (variant) {
         const [match, rest] = variant
         let matchPlatform = h.bracket(match) ?? ''
-        if (matchPlatform === '')
-          matchPlatform = (ctx.theme as any).platform?.[match] ?? ''
-        if (platform && platform.startsWith(matchPlatform)) {
+        const { platforms = {} } = (ctx.theme as any)
+        matchPlatform = matchPlatform === '' ? platforms[match] ?? '' : matchPlatform
+        if (matchPlatform && (platform === undefined || platform.startsWith(matchPlatform))) {
           return {
             matcher: rest,
           }
         }
       }
     },
-    autocomplete: 'uni-$platform:',
+    multiPass: true,
   }
 
   return [platformVariants]
