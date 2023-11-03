b82d2b0558409efab2a6c06ca748e4bbebc8cffd
life <36911513+Ares-Chang@users.noreply.github.com>
2023-11-03 17:31:59 +0800

    chore(deps): update unocss-applet package (#3)
    * chore(deps): update unocss-applet package
    
      Fixed an invalid unocss-applet dark model nesting syntax on wechat applet
    
    * chore: update unocss-applet

--- playground/src/pages/index.h5.vue
+++ playground/src/pages/index.h5.vue
@@ -1,6 +1,6 @@
 <template>
   <div container mx-auto py-10>
-    <view class="cursor-pointer border-1 inline-block p-2 rounded uni-web:mx-auto uni-h5:mx-auto">
+    <view class="cursor-pointer border-1 inline-block p-2 rounded uni-web:mx-auto uni-h5:mx-auto" bg="blue hover:yellow">
       h5 page
     </view>
   </div>
--- playground/src/pages/index.mp-weixin.vue
+++ playground/src/pages/index.mp-weixin.vue
@@ -1,6 +1,6 @@
 <template>
   <div container mx-auto py-10>
-    <view class="bg-blue cursor-pointer border-1 inline-block p-2 rounded uni-wechat:mx-auto uni-mp:mx-auto">
+    <view class="cursor-pointer border-1 inline-block p-2 rounded uni-wechat:mx-auto uni-mp:mx-auto" bg="blue hover:yellow">
       weixin page
     </view>
   </div>
