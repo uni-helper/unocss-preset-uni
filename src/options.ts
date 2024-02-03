6ffda0f0a704ccaad9ec0bea7ffabd124283538d
ModyQyW <wurui-dev@foxmail.com>
2024-02-03 20:42:38 +0800

    fix: fix option value parsing

--- src/options.ts
+++ src/options.ts
@@ -1,14 +1,17 @@
 import { isH5, isMp } from '@uni-helper/uni-env'
 import type { ResolvedUniPresetOptions, UniPresetOptions } from './types'
 
-function convertToObject<T>(value: T | boolean | undefined, defaultValue?: T) {
+function parseOption<T>(value: T | boolean | undefined, defaultValue?: T) {
+  if (value === false)
+    return value
+
   if (value === true)
     return defaultValue ?? {}
 
-  else if (value)
+  if (value)
     return { ...defaultValue, ...value }
 
-  else if (defaultValue)
+  if (defaultValue)
     return defaultValue
 
   return false
@@ -16,9 +19,15 @@ function convertToObject<T>(value: T | boolean | undefined, defaultValue?: T) {
 
 export function resolveOptions(userOptions: Partial<UniPresetOptions> = {}): ResolvedUniPresetOptions {
   const dark = isH5 ? 'class' : 'media'
-  const uno = convertToObject(userOptions.uno, { dark })
-  const attributify = convertToObject(userOptions.attributify, { ignoreAttributes: isH5 ? undefined : ['block', 'fixed'] })
-  const remRpx = convertToObject(userOptions.remRpx, { mode: isMp ? undefined : 'rpx2rem' })
+  const uno = parseOption(userOptions.uno, { dark })
+  const attributify = parseOption(
+    userOptions.attributify,
+    { ignoreAttributes: isH5 ? undefined : ['block', 'fixed'] },
+  )
+  const remRpx = parseOption(
+    userOptions.remRpx,
+    { mode: isMp ? undefined : 'rpx2rem' },
+  )
 
   return {
     ...userOptions,
