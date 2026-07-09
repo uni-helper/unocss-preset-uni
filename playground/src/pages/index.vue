bd55d58d1d56036618023d30950576fe7b9786cd
Panpan <wurui.dev@gmail.com>
2026-07-09 21:26:31 +0900

    chore: lint fix

--- playground/src/pages/index.vue
+++ playground/src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div container mx-auto py-10>
-    <view class="inline-block cursor-pointer border-1 rounded from-red-500 to-pink-500 bg-gradient-to-r p-2 uni-mp:mx-auto uni-wechat:mx-auto" bg="blue hover:yellow">
+    <view class="uni-mp:mx-auto uni-wechat:mx-auto inline-block cursor-pointer border-1 rounded from-red-500 to-pink-500 bg-gradient-to-r p-2" bg="blue hover:yellow">
       Hello UniApp
     </view>
   </div>
